e333b1ea146ddff0454db2442490db76164798e3
Faiz Khan <faiz.khan@everestek.com>
2024-06-24 12:33:38 +0530

    feat: lint-staged check

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,37 @@
+import { json } from "@remix-run/node";
+import { createRemixStub } from "@remix-run/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { describe, expect, it } from "vitest";
+import HomePage from "~/routes/_index";
+
+describe("Home Page", () => {
+  const user = userEvent.setup();
+  const RemixStub = createRemixStub([
+    {
+      path: "/",
+      Component: HomePage,
+      loader() {
+        return json({ message: "hello from loader" });
+      },
+    },
+  ]);
+  render(<RemixStub />);
+
+  it('should render the "hello from loader" message', async () => {
+    await waitFor(() => screen.getByText("hello from loader"));
+  });
+
+  it("should create a new note when click the button", async () => {
+    await waitFor(async () => {
+      const createNoteButton = screen.getByRole("button", {
+        name: "Create a Note",
+      });
+
+      await user.click(createNoteButton);
+      const notesList = screen.getByRole("list");
+
+      expect(notesList).toContain(screen.getByText("Note 2"));
+    });
+  });
+});
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+
+export async function loader() {
+  return {
+    message: "hello from loader",
+  };
+}
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,41 +16,37 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const loaderData = useLoaderData<typeof loader>();
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "This is a note",
+    },
+  ]);
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
+    <main className="container">
+      <h1>Welcome to Remix</h1>
+      <p>{loaderData.message}</p>
+      <button
+        type="button"
+        onClick={() =>
+          setNotes([
+            ...notes,
+            {
+              id: notes.length + 1,
+              text: `Note ${notes.length + 1}`,
+            },
+          ])
+        }
+      >
+        Create a Note
+      </button>
+      <ul>
+        {notes.map((note) => {
+          return <li key={note.id}>{note.text}</li>;
+        })}
       </ul>
-    </div>
+    </main>
   );
 }
